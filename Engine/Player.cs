9efe9f17b392982767b23d570ecbf7b684a644f4
Ethan Simmonds <esimmonds2@outlook.com>
2018-11-24 20:01:00 +1300

    Added logic for inventory and quest checking and reward handlers

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -13,6 +13,7 @@ namespace Engine
         public int Level { get; set; }
         public List<InventoryItem> Inventory { get; set; }
         public List<PlayerQuest> Quests;
+        public Location CurrentLocation { get; set; }
 
         public Player(int currentHP, int maxHP, int gold, int xp, int level) : base(currentHP, maxHP)
         {
--- dossier/Manager.cs
+++ dossier/Manager.cs
@@ -13,7 +13,8 @@ namespace dossier
 {
     public partial class Manager : Form
     {
-        private Player _player;       
+        private Player _player;
+        private Monster _currentmonster;
         public Manager()
         {
             InitializeComponent();
@@ -22,8 +23,307 @@ namespace dossier
             labelGold.Text = _player.Gold.ToString();
             labelEXP.Text = _player.Experience.ToString();
             labelLevel.Text = _player.Level.ToString();
+        }
 
-            
+        private void MoveTo(Location newLocation)
+        {
+            // Check to determine if the location has any items required to enter
+            if (newLocation.ItemRequired != null)
+            {
+                bool playerHasItem = false;
+                // Check each item in invetory and compare their IDs
+                foreach (InventoryItem item in _player.Inventory)
+                {
+                    if (item.Details.ID == newLocation.ItemRequired.ID)
+                    {
+                        playerHasItem = true;
+                        break;                    
+                    }
+                }
+                // If the item wasn't found in inventory
+                if (!playerHasItem)
+                {
+                    rtbMessages.Text = "You must have " + newLocation.ItemRequired.Name + " to enter here." + Environment.NewLine;
+                    return;
+                }
+            }
+            // Set current location of player to the new location, assuming item requirements are met
+            _player.CurrentLocation = newLocation;
+
+            // Set visibility of directional buttons
+            btnNorth.Visible = (newLocation.LocationNorth != null);
+            btnSouth.Visible = (newLocation.LocationSouth != null);
+            btnWest.Visible = (newLocation.LocationWest != null);
+            btnEast.Visible = (newLocation.LocationEast != null);
+
+            // Display current player location with accompanying description
+            rtbLocation.Text = newLocation.Name + Environment.NewLine;
+            rtbLocation.Text += newLocation.Description + Environment.NewLine;
+
+            // This logic will enable zone changes to completely heal the player. Should exclude this as it's 
+            // ridiculously cheesable
+            //_player.CurrentHP = _player.MaxHP;
+            //passiveLabelHP.Text = _player.CurrentHP.ToString();
+
+            // Check locations if there is an available quest there
+            if (newLocation.QuestAvailable != null)
+            {
+                bool playerHasQuest = false;
+                bool playerHasCompletedQuest = false;
+
+                // Check each quest in the players quest log via ID comparison
+                foreach (PlayerQuest quest in _player.Quests)
+                {
+                    if(quest.Details.ID == newLocation.QuestAvailable.ID)
+                    {
+                        playerHasQuest = true;
+                        // Check for completion
+                        if (quest.IsCompleted == true)
+                        {
+                            playerHasCompletedQuest = true;
+                        }
+                    }
+                }
+                // If the player has the quest, but is yet to complete it, run this routine
+                if (playerHasQuest)
+                {
+                    if (!playerHasCompletedQuest)
+                    {
+                        bool playerHasItemsToComplete = true;
+                        // Check quest items for quests available in the location
+                        foreach (QuestCompletionItem qci in newLocation.QuestAvailable.QuestCompletionItems)
+                        {
+                            bool foundInInventory = false;
+                            foreach (InventoryItem ii in _player.Inventory)
+                            {
+                                // Item check
+                                if (ii.Details.ID == qci.Details.ID)
+                                {
+                                    foundInInventory = true;
+                                    // Item quant check
+                                    // If the player has less items than required, set to false and break
+                                    if (ii.Quantity < qci.Quantity)
+                                    {
+                                        playerHasItemsToComplete = false;
+                                        // Eliminates extra checking effort for other required quest items, as
+                                        // the conditions have already not been met
+                                        break;
+
+                                    }
+                                    // Break for every other quantity check case (equal or greater amount of quantity)
+                                    break;
+                                }
+                            }
+                            // Check if item was found. If not stop looking for others
+                            if (!foundInInventory)
+                            {
+                                playerHasItemsToComplete = false;
+                                break;
+                            }
+                        }
+                        // If the player has all items to complete the quest
+                        if (playerHasItemsToComplete)
+                        {
+                            // Print result to manager
+                            rtbMessages.Text += Environment.NewLine;
+                            rtbMessages.Text += newLocation.QuestAvailable.Name + " completed" + Environment.NewLine;
+
+                            // Remove quest items from player inventory, iterate through the required items
+                            // for the quest existing in the zone
+                            foreach (QuestCompletionItem qci in newLocation.QuestAvailable.QuestCompletionItems)
+                            {
+                                // Remove each item in the required quantity
+                                foreach (InventoryItem ii in _player.Inventory)
+                                {
+                                    if (ii.Details.ID == qci.Details.ID)
+                                    {
+                                        // Subtract the amount of items needed by the quest in the player inventory 
+                                        // by the amount required for the quest. This makes excess items collected by
+                                        // the player to still remain in their inventory after the quest is completed
+                                        ii.Quantity -= qci.Quantity;
+                                        break;
+                                    }
+                                }                               
+                            }
+                            // Quest rewards
+                            rtbMessages.Text += "You receive: " + Environment.NewLine;
+                            // XP 
+                            rtbMessages.Text += newLocation.QuestAvailable.RewardXP.ToString() + " experience points" + Environment.NewLine;
+                            // Gold
+                            rtbMessages.Text += newLocation.QuestAvailable.RewardGold.ToString() + " gold" + Environment.NewLine;
+                            // Items
+                            rtbMessages.Text += newLocation.QuestAvailable.RewardItem.Name.ToString() + Environment.NewLine;
+                            // Append experience and gold to player instances
+                            _player.Experience += newLocation.QuestAvailable.RewardXP;
+                            _player.Gold += newLocation.QuestAvailable.RewardGold;
+
+                            // Add any items gained as a reward to the player inventory
+                            bool addedToInventory = false;
+                            foreach(InventoryItem ii in _player.Inventory){
+                                // Check to see if item already exists in the current inventory context  
+                                // If so increase the quantity by one
+                                if (ii.Details.ID == newLocation.QuestAvailable.RewardItem.ID)
+                                {
+                                    ii.Quantity++;
+                                    addedToInventory = true;
+                                    break;
+                                }                                                              
+                            }
+                            // If reward doesn't exist in the inventory
+                            if (!addedToInventory)
+                            {
+                                _player.Inventory.Add(new InventoryItem(newLocation.QuestAvailable.RewardItem, 1));
+                            }
+                            // Mark quest as completed
+                            foreach (PlayerQuest pq in _player.Quests)
+                            {
+                                if (pq.Details.ID == newLocation.QuestAvailable.ID)
+                                {
+                                    pq.IsCompleted = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                // If the player does NOT have the quest, run this routine
+                else
+                {
+                    rtbMessages.Text += "You receive the " + newLocation.QuestAvailable.Name + " quest." + Environment.NewLine;
+                    rtbMessages.Text += newLocation.QuestAvailable.Description + Environment.NewLine;
+                    rtbMessages.Text += "To complete, return with " + Environment.NewLine;
+                    foreach (QuestCompletionItem qci in newLocation.QuestAvailable.QuestCompletionItems)
+                    {
+                        if (qci.Quantity == 1)
+                        {
+                            rtbMessages.Text += qci.Quantity.ToString() + " " + qci.Details.Name + Environment.NewLine;
+                        }
+                        else
+                        {
+                            rtbMessages.Text += qci.Quantity.ToString() + " " + qci.Details.NamePlural + Environment.NewLine;
+                        }
+                    }
+                    rtbMessages.Text += Environment.NewLine;
+                    // Add quest to the players quest list
+                    _player.Quests.Add(new PlayerQuest(newLocation.QuestAvailable));
+                }
+            }
+            // if a monster exists in this zone, 
+            if (newLocation.MonsterHere != null)
+            {
+                rtbMessages.Text += "You see a " + newLocation.MonsterHere.Name + Environment.NewLine;
+                // Init monster, pull values from World instance
+                Monster standardMonster = World.MonsterByID(newLocation.MonsterHere.ID);
+                _currentmonster = new Monster(standardMonster.ID, standardMonster.Name, standardMonster.MaxDamage, 
+                    standardMonster.RewardXP, standardMonster.RewardGold, standardMonster.CurrentHP, standardMonster.MaxHP);
+                // Pull loot table values to monster
+                foreach (LootItem lootitem in standardMonster.LootTable)
+                {
+                    _currentmonster.LootTable.Add(lootitem);
+                }
+                cboWeapons.Visible = true;
+                cboPotions.Visible = true;
+                btnUsePotion.Visible = true;
+                btnUseWeapon.Visible = true;
+            }
+            // If monster doesn't exist in this zone. set to null
+            else
+            {
+                _currentmonster = null;
+                cboWeapons.Visible = false;
+                cboPotions.Visible = false;
+                btnUsePotion.Visible = false;
+                btnUseWeapon.Visible = false;
+            }
+            // Refresh inventory list
+            dgvInventory.RowHeadersVisible = false;
+            dgvInventory.ColumnCount = 2;
+            dgvInventory.Columns[0].Name = "Name";
+            dgvInventory.Columns[0].Width = 197;
+            dgvInventory.Columns[1].Name = "Quantity";
+            dgvInventory.Rows.Clear();
+
+            foreach(InventoryItem item in _player.Inventory)
+            {
+                if (item.Quantity > 0)
+                {
+                    dgvInventory.Rows.Add(new[] { item.Details.Name, item.Quantity.ToString() });
+                }
+            }
+
+            // Refresh quest list
+            dgvQuests.RowHeadersVisible = false;
+            dgvInventory.ColumnCount = 2;
+            dgvInventory.Columns[0].Name = "Name";
+            dgvInventory.Columns[0].Width = 197;
+            dgvInventory.Columns[1].Name = "Done?";
+
+            dgvInventory.Rows.Clear();
+
+            foreach (PlayerQuest quest in _player.Quests)
+            {
+                dgvQuests.Rows.Add(new[] { quest.Details.Name, quest.IsCompleted.ToString() });             
+            }
+
+            // Refresh combo box for players weapons
+            List<Weapon> weapons = new List<Weapon>();
+            foreach (InventoryItem item in _player.Inventory)
+            {
+                if (item.Details is Weapon)
+                {
+                    if (item.Quantity > 0)
+                    {
+                        weapons.Add((Weapon)item.Details);
+                    }                 
+                }
+            }
+            // If player has no weapons, hide combobox and use button
+            if (weapons.Count == 0)
+            {
+                cboWeapons.Visible = false;
+                btnUseWeapon.Visible = false;
+            }
+            else
+            {
+                cboWeapons.DataSource = weapons;
+                cboWeapons.DisplayMember = "Name";
+                cboWeapons.ValueMember = "ID";
+                cboWeapons.SelectedIndex = 0;
+            }
+
+            // Refresh healing potions combo box
+            List<HealingPotion> potions = new List<HealingPotion>();
+            foreach (InventoryItem item in _player.Inventory)
+            {
+                if (item.Details is HealingPotion)
+                {
+                    if (item.Quantity > 0)
+                    {
+                        potions.Add((HealingPotion)item.Details);
+                    }
+                }
+            }
+            // If player has no potions, hide combobox and use button
+            if (potions.Count == 0)
+            {
+                cboPotions.Visible = false;
+                btnUsePotion.Visible = false;
+            }
+            else              
+            {             
+                cboPotions.DataSource = potions;
+                cboPotions.DisplayMember ="Name";               
+                cboPotions.ValueMember = "ID";               
+                cboPotions.SelectedIndex = 0;
+            }
+        }
+        private void btnUseWeapon_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnUsePotion_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
